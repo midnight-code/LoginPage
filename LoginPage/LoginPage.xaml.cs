b32889f78f4fb9a1718d950ce7c548e2c78b9a21
Evgenii Lewicki <midnight-code@ya.ru>
2022-07-20 20:23:13 +0300

    Add page Login and Register.

---
+++ LoginPage/LoginPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(txtUserName.Text=="admin" && txtUserPassword.Text == "1234")
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                DisplayAlert("Ошибка!", "Неверный логин пароль!","OK");
+            }
+        }
+    }
+}
